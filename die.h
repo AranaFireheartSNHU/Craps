b655d29f93185a9de5e57880c7f6368b4ef34aee
DragonFired <aranafireheart@mac.com>
2020-02-14 13:46:33 -0500

    End of class die class incomplete.

--- die.h
+++ die.h
@@ -5,4 +5,34 @@
 #ifndef CRAPS_DIE_H
 #define CRAPS_DIE_H
 
+#include <string>
+
+class Die {
+private:
+    int numberOfSides;
+    int increment;
+    int startingValue;
+    int value;
+    std::string color;
+    bool isSymbols;
+    std::string material;
+public:
+    Die (int startingNumberOfSides, int startingIncrement=1, int startingStartingValue=1, std::string color="White", bool isSymbols=false, std::string material="Bone");
+    int roll();
+    void setNumberOfSides(int newNumberOfSides);
+    int getNumberOfSides();
+    void setIncrement(int newIncrement);
+    int getIncrement();
+    void setStartingValue(int newStartingValue);
+    int getStartingValue();
+    void setValue(int newValue);
+    int getValue();
+    void setColor(std::string newColor);
+    std::string getColor();
+    void setIsSymbol(bool newIsSymbol);
+    bool isSymbol();
+    void setMaterial(std::string newMaterial);
+    std::string getMaterial();
+};
+
 #endif //CRAPS_DIE_H
