3fc7ddee1b66167cbd712d9665281add1fc7ae67
DragonFired <aranafireheart@mac.com>
2020-02-14 13:06:20 -0500

    Added die class

---
+++ die.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Arana Fireheart on 2/14/20.
+//
+
+#include "die.h"
---
+++ die.h
@@ -0,0 +1,8 @@
+//
+// Created by Arana Fireheart on 2/14/20.
+//
+
+#ifndef CRAPS_DIE_H
+#define CRAPS_DIE_H
+
+#endif //CRAPS_DIE_H
